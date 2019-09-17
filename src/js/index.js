b57cc0d0135b48937a6f4316f7e0bd28de0c6177
expert2013 <36111564+expert2013@users.noreply.github.com>
2019-09-17 12:15:54 +0300

    Mobile-menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
 import "./import/modules";
+
+
+$(document).ready(function(){
+
+
+	$('.open-button').click(function(){
+		$(this).toggleClass('open');
+	});
+
+ 
+	$(".open-button").click(function(e){
+			e.preventDefault();
+			$(".mobile-menu").toggleClass('open');
+	});
+
+});
